72e1439cd29ac1c0527bd00241951710251ea115
Aurelie Moustardier <MOUSTARA@FILIERIS.SANTE>
2024-09-26 16:41:42 +0200

    exercice 1

--- shop/serializer.py
+++ shop/serializer.py
@@ -1,11 +1,16 @@
 
 from rest_framework.serializers import ModelSerializer
 
-from shop.models import Category
+from shop.models import Category, Product
 
 class CategorySerializer(ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ['id', 'date_created', 'date_updated', 'name']
 
+class ProductSerializer(ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'date_created', 'date_updated', 'category']
--- shop/views.py
+++ shop/views.py
@@ -1,15 +1,17 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from shop.models import Category
-from shop.serializer import CategorySerializer
+from shop.models import Category, Product
+from shop.serializer import CategorySerializer, ProductSerializer
 
 class CategoryAPIViews(APIView):
     def get(self, *args, **kwargs):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset, many=True)
         return Response(serializer.data)
 
 class ProductAPIViews(APIView):
     def get(self, *arg, **kwargs):
-        identifiant =
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
